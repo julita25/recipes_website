83de75ac7d4e488ec3e8e5238b4e219f991d49de
juli <julitati25@gmail.com>
2022-11-02 22:40:23 +0400

    rename component

---
+++ components/RecipeCards/RecipeCards.jsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { Message, Panel, Tag } from "rsuite";
+import { bool, string } from "prop-types";
+import UrlButton from "../UrlButton/UrlButton";
+import PageLoader from "../PageLoader/PageLoader";
+import { APP_ID, APP_KEY } from "../../constants/apiConstants";
+
+const RecipeCards = ({ filters, isGlutenFree, searchRecipe }) => {
+  const router = useRouter();
+  const pathname = router.asPath;
+  const [recipes, setRecipes] = useState();
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState();
+  const [error, setError] = useState();
+
+  const fetchRecipes = async (healthLabel, filter = "All") => {
+    setLoading(true);
+    const res = await fetch(
+      `https://api.edamam.com/api/recipes/v2?type=public&q=${searchRecipe}${filter !== "All" ? `&diet=${filters.toLowerCase()}` : ""}${healthLabel ? "&health=gluten-free" : ""}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    if (res?.ok) {
+      const arr = [];
+      const response = await res.json();
+      const { hits } = response;
+      hits.forEach((item) => {
+        const obj = { ...item.recipe };
+        arr.push(obj);
+      });
+      setRecipes(arr);
+      setLoading(false);
+      setError();
+    } else {
+      const response = await res.json();
+      setLoading(false);
+      setError(response?.message || new Error("Recipe not available"));
+    }
+  };
+
+  useEffect(() => {
+    if (isGlutenFree) {
+      fetchRecipes(isGlutenFree, filters);
+    } else {
+      fetchRecipes(false, filters);
+    }
+  }, [searchRecipe, filters, isGlutenFree]);
+
+  useEffect(() => {
+    if (!recipes?.length) {
+      setMessage("No recipes found");
+    } else {
+      setMessage();
+    }
+  }, [recipes]);
+
+  if (loading) return <PageLoader />;
+
+  return (
+    <div className="flex flex-col space-y-5">
+      {message && <Message type="error" showIcon header={message} />}
+      {error && <Message type="error" showIcon header={error} />}
+      <div className="grid grid-cols-4 gap-5">
+        {recipes?.map(({
+          label, image, mealType, uri
+        }) => (
+          <Panel
+            shaded
+            bordered
+            bodyFill
+            className="w-full h-42"
+          >
+            <div>
+              <Tag className="absolute" color="green">{mealType}</Tag>
+              <img src={image} className="w-full h-42 object-cover" alt={label} />
+            </div>
+            <Panel
+              className="bg-yellow-700 h-28 rounded-none"
+              header={(
+                <div className="flex flex-col space-y-2 w-full">
+                  <div className="flex text-white">{label}</div>
+                  <UrlButton url={`${pathname}/recipes/${uri?.split("#")[1]}`} color="yellow" className="bg-yellow-500">Details</UrlButton>
+                </div>
+              )}
+            />
+
+          </Panel>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+RecipeCards.propTypes = {
+  filters: string.isRequired,
+  isGlutenFree: bool.isRequired,
+  searchRecipe: string
+};
+
+RecipeCards.defaultProps = {
+  searchRecipe: ""
+};
+
+export default RecipeCards;
--- pages/recipes/[recipeId].jsx
+++ pages/recipes/[recipeId].jsx
@@ -1,48 +1,50 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { Panel, Tag } from "rsuite";
-import { ArrowRightLine } from "@rsuite/icons"
-import PageLoader from "/components/PageLoader/PageLoader";
-import NutritionList from "/components/NutritionList/NutritionList";
-import UrlButton from "/components/UrlButton/UrlButton";
+import { Message, Panel, Tag } from "rsuite";
+import { ArrowRightLine } from "@rsuite/icons";
+import PageLoader from "../../components/PageLoader/PageLoader";
+import NutritionList from "../../components/NutritionList/NutritionList";
+import UrlButton from "../../components/UrlButton/UrlButton";
 
 const RecipeDetails = () => {
   const router = useRouter();
   const { recipeId } = router.query;
-  console.log(recipeId);
-  const [foodRecipe, setFoodRecipe] = useState()
-  const [error, setError] = useState()
+  const [foodRecipe, setFoodRecipe] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
 
   const fetchRecipeById = async () => {
-    const res = await fetch(`https://api.edamam.com/api/recipes/v2/${recipeId}?type=public&app_id=737ad155&app_key=fc1f67d30775e8e666e35dfeb0f07c0e`)
+    const res = await fetch(`https://api.edamam.com/api/recipes/v2/${recipeId}?type=public&app_id=737ad155&app_key=fc1f67d30775e8e666e35dfeb0f07c0e`);
     if (res?.ok) {
       const response = await res.json();
       const { recipe } = response;
       setFoodRecipe(recipe);
+      setLoading(false);
+      setError();
     } else {
       const response = await res.json();
       setError(response?.message || new Error("Error getting data"));
-
+      setLoading(false);
     }
-  }
-
-  console.log(foodRecipe);
+  };
 
   useEffect(() => {
     if (recipeId) {
-      // fetchRecipeById();
+      fetchRecipeById();
     }
   }, [recipeId]);
 
-  if (!foodRecipe) return <PageLoader />
+  if (loading) return <PageLoader />;
 
   return (
     <>
+      {error && <Message type="error" showIcon header={error} />}
       <div className="bg-yellow-700 h-42 p-5 space-y-10">
         <div className="text-white text-3xl">{foodRecipe.label}</div>
         <div className="flex space-x-10">
           <div className="text-xl text-white">
-            Calories: {Math.round(foodRecipe.calories)}
+            Calories:
+            {Math.round(foodRecipe.calories)}
           </div>
           <div className="flex text-xl text-white space-x-2">
             <div>Dish type: </div>
@@ -56,18 +58,21 @@ const RecipeDetails = () => {
       </div>
       <div className="flex p-4 space-x-5">
         <div className="w-1/3">
-          <img className="w-full" src={foodRecipe.image} />
+          <img className="w-full" src={foodRecipe.image} alt={foodRecipe.label} />
           <Panel
             header="Ingredients"
             className="bg-white"
           >
-            {foodRecipe.ingredientLines.map((item) =>
-              <div><ArrowRightLine />{item}</div>
-            )}
+            {foodRecipe.ingredientLines.map((item) => (
+              <div>
+                <ArrowRightLine />
+                {item}
+              </div>
+            ))}
           </Panel>
           <div className="mt-2">
-            <Panel header="Recipe link" className="bg-white">
-              <UrlButton url={foodRecipe.url} />
+            <Panel header="Full recipe" className="bg-white">
+              <UrlButton url={foodRecipe.url}>Follow link</UrlButton>
             </Panel>
           </div>
         </div>
@@ -76,7 +81,7 @@ const RecipeDetails = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default RecipeDetails
+export default RecipeDetails;
