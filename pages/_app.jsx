0559c88e1d285a4ecf7ef24f496ef98b9536e78c
juli <julitati25@gmail.com>
2022-11-02 23:32:26 +0400

    fix lint errors

---
+++ pages/_app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { instanceOf } from "prop-types";
+import "../styles/globals.css";
+
+const App = ({ Component, pageProps }) => <Component {...pageProps} />
+
+export default App;
+
+App.propTypes = {
+  Component: instanceOf(Object).isRequired,
+  pageProps: instanceOf(Object).isRequired
+};
